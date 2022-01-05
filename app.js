8dc1ac3c39a1d44c2e07cbea641724beaafb55e7
wblanck <blanck.william@gmail.com>
2022-01-05 17:31:06 +0100

    added api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // https://random-word-api.herokuapp.com/word?number=1 get random word
 // https://rapidapi.com/community/api/urban-dictionary/ get definition
 
-/* const getRandomWord = async () => {
+const getRandomWord = async () => {
   const base = "https://random-words-api.vercel.app/word";
 
   const response = await fetch(base);
@@ -11,80 +11,88 @@
 };
 
 getRandomWord()
-  .then((data) => console.log(data.word + " => definition: " + data.definition))
-  .catch((error) => console.log(error));
- */
-const chars =
-  "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z".split(
-    ", "
-  );
-const word = "hello".toUpperCase();
-const answerContainer = document.querySelector(".answer");
-const charsContainer = document.querySelector(".chars");
-const displayContainer = document.querySelector(".display");
+  .then((data) => {
+    const chars =
+      "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z".split(
+        ", "
+      );
+    const word = data.word.toUpperCase();
+    const definition = data.definition;
 
-let livesLeft = 9;
-let correctCharsLeft = word.length;
+    const answerContainer = document.querySelector(".answer");
+    const charsContainer = document.querySelector(".chars");
+    const displayContainer = document.querySelector(".display");
 
-const renderElements = () => {
-  let charsHtml = "";
-  let answerHtml = "";
-  let livesHtml = "";
+    let livesLeft = 9;
+    let correctCharsLeft = word.length;
 
-  chars.forEach((char) => {
-    charsHtml += `<div class="char">${char}</div>`;
-  });
+    const renderElements = () => {
+      let charsHtml = "";
+      let answerHtml = "";
+      let livesHtml = "";
 
-  charsContainer.innerHTML = charsHtml;
-  for (let i = 0; i < word.length; i++) {
-    answerHtml += `<div class="correct-char underline"></div>`;
-  }
-  answerContainer.innerHTML = answerHtml;
+      chars.forEach((char) => {
+        charsHtml += `<div class="char">${char}</div>`;
+      });
 
-  for (let i = 0; i < livesLeft; i++) {
-    livesHtml += ` <i class="fas fa-heart"></i>`;
-  }
-  displayContainer.innerHTML = livesHtml;
-};
-
-const checkChar = (char, element) => {
-  // if word contains the clicked char, loop through the correct word char by char, compare clicked char to correct char if true display it
-  // else livesLeft--
-  if (word.includes(char)) {
-    for (let i = 0; i < word.length; i++) {
-      if (char === word[i]) {
-        correctCharsLeft--;
-        if (!correctCharsLeft) {
-          console.log("you won");
-        }
-        const correctCharElement = answerContainer.children[i];
-
-        correctCharElement.textContent = char;
-        correctCharElement.classList.remove("underline");
-        element.classList.add("fade-out--correct");
+      charsContainer.innerHTML = charsHtml;
+      for (let i = 0; i < word.length; i++) {
+        answerHtml += `<div class="correct-char underline"></div>`;
       }
-    }
-  } else {
-    element.classList.add("fade-out--wrong");
-    livesLeft--;
-    displayContainer.children[0].remove();
-    if (!livesLeft) {
-      console.log("you lose buddy");
-      element.classList.add("fade-out--wrong");
-    } else {
-      console.log(`lives: ${livesLeft} left`);
-    }
-  }
-};
-charsContainer.addEventListener("click", (e) => {
-  const char = e.target.textContent;
-  const clickedAChar = char.length <= 1;
-  const gotLives = livesLeft > 0;
-  const notWonYet = correctCharsLeft != 0;
-  const notPressedBefore = e.target.classList.length <= 1;
+      answerContainer.innerHTML = answerHtml;
 
-  if (clickedAChar && gotLives && notWonYet && notPressedBefore) {
-    checkChar(char, e.target);
-  }
-});
-renderElements();
+      for (let i = 0; i < livesLeft; i++) {
+        livesHtml += ` <i class="fas fa-heart"></i>`;
+      }
+      displayContainer.innerHTML = livesHtml;
+    };
+
+    const checkChar = (char, element) => {
+      // if word contains the clicked char, loop through the correct word char by char, compare clicked char to correct char if true display it
+      // else livesLeft--
+      if (word.includes(char)) {
+        for (let i = 0; i < word.length; i++) {
+          if (char === word[i]) {
+            correctCharsLeft--;
+            if (!correctCharsLeft) {
+              console.log("you won");
+            }
+            const correctCharElement = answerContainer.children[i];
+
+            correctCharElement.textContent = char;
+            correctCharElement.classList.remove("underline");
+            element.classList.add("fade-out--correct");
+          }
+        }
+      } else {
+        element.classList.add("fade-out--wrong");
+        livesLeft--;
+        displayContainer.children[0].remove();
+        if (!livesLeft) {
+          console.log("you lose buddy");
+          element.classList.add("fade-out--wrong");
+        } else {
+          console.log(`lives: ${livesLeft} left`);
+        }
+      }
+    };
+    charsContainer.addEventListener("click", (e) => {
+      console.log(word);
+      const char = e.target.textContent;
+      const clickedAChar = char.length <= 1;
+      const gotLives = livesLeft > 0;
+      const notWonYet = correctCharsLeft != 0;
+      const notPressedBefore = e.target.classList.length <= 1;
+
+      console.log(char, clickedAChar, gotLives, notWonYet, notPressedBefore);
+      if (clickedAChar && gotLives && notWonYet && notPressedBefore) {
+        console.log("check char");
+        checkChar(char, e.target);
+      } else {
+        console.log("wrong");
+      }
+    });
+
+    renderElements();
+  })
+  .catch((error) => console.log(error));
